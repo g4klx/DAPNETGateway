6284930b307846b44f76590245e82fd1a39d0493
Jonathan Naylor <naylorjs@yahoo.com>
2018-07-19 18:07:25 +0100

    Change the time to expire delayed time messages to 15 seconds after advice.

--- DAPNETGateway.cpp
+++ DAPNETGateway.cpp
@@ -69,7 +69,7 @@ const unsigned char FUNCTIONAL_ALERT1       = 1U;
 const unsigned char FUNCTIONAL_ALERT2       = 2U;
 const unsigned char FUNCTIONAL_ALPHANUMERIC = 3U;
 
-const unsigned int MAX_TIME_TO_HOLD_TIME_MESSAGES = 30000U;		// 30s
+const unsigned int MAX_TIME_TO_HOLD_TIME_MESSAGES = 15000U;		// 15s
 
 int main(int argc, char** argv)
 {
