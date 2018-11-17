54f3cd72817bdadfbf3e7298ee9c60d1bed5cfc3
Simon <simon@adlem.net>
2018-11-17 16:56:46 +0000

    Header

--- REGEX.cpp
+++ REGEX.cpp
@@ -1,6 +1,8 @@
 /*
 *   Copyright (C) 2016,2017 by Jonathan Naylor G4KLX
 *
+*   Contributed by Simon G7RZU
+*
 *   This program is free software; you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
 *   the Free Software Foundation; either version 2 of the License, or
